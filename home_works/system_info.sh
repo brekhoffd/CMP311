3d0904b912d86c8f424bdde325338bb1c1c344ac
Denys <denys.brekhov@gmail.com>
2024-10-21 09:50:09 +0300

    Upload system_info.sh

---
+++ home_works/system_info.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+clear
+
+# Home work:
+echo "***********************"
+echo "HOMEWORK FOR 26/10/2024"
+echo "***********************"
+
+# Current user:
+echo
+echo "Current user:"
+whoami
+
+# The name and version of the operating system:
+echo
+echo "The name and version of the operating system:"
+uname -a
+
+# The amount of free disk space:
+echo
+echo "The amount of free disk space:"
+df -h
+
+# List of active users on the system:
+echo
+echo "List of active users on the system:"
+w
+
+# Completion message:
+echo
+echo "End of system info!"
+echo
